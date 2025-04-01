9e5a4056c3f4b3a904b237580781d95d3f2b745b
simplescholar1 <mohamedytaha@aucegypt.edu>
2025-04-01 22:34:11 +0200

    calculator.cpp finished

---
+++ cs2assignment/calculator.h
@@ -0,0 +1,8 @@
+double add(double a, double b);
+double sub(double a, double b);
+double mult(double a, double b);
+double division (double a, double b);
+int fact(int a, int b);
+ int GCD( int a, int b);
+ int LCM(int a,int b)
+ int RNG(int strt_raneg, int endrange)
